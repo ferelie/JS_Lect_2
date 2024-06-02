4e65920956aa72ec4222a69578b6d6cb6f441a83
ferelie <abdallah.yasser0501@gmail.com>
2024-06-02 23:09:49 +0300

    final edits

--- Task_4/script.js
+++ Task_4/script.js
@@ -1,15 +1,12 @@
 let radius = prompt("What is the value of your circle's radius?");
-alert("The area of your circle is " + Math.PI * radius ** 2);
+alert(`The area of your circle is ${Math.PI * radius ** 2}`);
 
 let squared = prompt(
     "What is the value you want to calculate the square root of?"
 );
-alert("The square root of " + squared + " is " + Math.sqrt(squared));
+alert(`The square root of ${squared} is ${Math.sqrt(squared)}`);
 
 let angle = prompt("What is the value of your angle?");
-document.body.innerHTML =
-    document.body.innerHTML +
-    "The cosine of " +
-    angle +
-    " is " +
-    Math.cos(angle);
+document.body.innerHTML += `<p>The cosine of ${angle} is ${Math.cos(
+    angle
+)}</p>`;
